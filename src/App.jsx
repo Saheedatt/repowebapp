4843b891a81cf2735acd81c28cd59d6e98b2900e
Saheedatt <afolabisaheedat1@gmail.com>
2024-04-18 21:11:50 +0100

    Still failing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 import RespositoryList from "./components/RepositoryList.jsx";
 import RepositoryDetails from "./components/RepositoryDetails.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
@@ -11,6 +16,19 @@ import Projects from "./components/Projects.jsx";
 import "./index.css";
 import Random from "./components/Random.jsx";
 
+function Layout({ user }) {
+  return (
+    <div className="flex h-full">
+      <Sidebar user={user} />
+      <div className="container h-screen flex-1 flex flex-col shadow-lg">
+        <Header />
+        <div className="main flex-1 p-10 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
 function App() {
   const username = "saheedatt";
 
@@ -31,32 +49,70 @@ function App() {
       },
     ],
   };
-
-  return (
-    <div className="flex h-full">
-      <Sidebar user={user} />
-      <div className="container h-screen flex-1 flex flex-col shadow-lg">
-        <Header />
-        <div className="main flex-1 p-10 overflow-y-auto">
-          <Routes>
-            <Route path="/" element={<RespositoryList username={username} />} />
-            <Route path="/overview" element={<Overview />} />
-            <Route path="/repository/:repoId" element={<RepositoryDetails />} />
-            <Route
-              path="/random"
-              element={
-                <ErrorBoundary>
-                  <Random />
-                </ErrorBoundary>
-              }
-            />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
+  return(
+    <Routes>
+      <Route path="/" element={<Layout user={user} />}>
+      <Route index element={<RespositoryList username={username} />} />
+        <Route path="overview" element={<Overview />} />
+        <Route path="repository/:repoId" element={<RepositoryDetails />} />
+        <Route path="random" element={
+          <ErrorBoundary>
+            <Random />
+          </ErrorBoundary>
+        } />
+        <Route path="projects" element={<Projects />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
+  )
 }
-
 export default App;
+// function App() {
+//   const username = "saheedatt";
+
+//   const user = {
+//     name: "Saheedat Afolabi",
+//     username: "saheedatt",
+//     bio: "Building in an attempt to solve important issues. Attempting technical writing. Physiotherapy student.",
+//     avatarUrl:
+//       "https://avatars.githubusercontent.com/u/104633466?s=400&u=b88cebf8586db7687d5cb875244891137d7135c9&v=4",
+//     location: "Lagos, Nigeria",
+//     socialLinks: [
+//       { name: "GitHub", url: "https://github.com/Saheedatt" },
+//       { name: "X(Twitter)", url: "https://twitter.com/idontseemyselff" },
+//       { name: "Email", url: "mailto:afolabisaheedat1@gmail.com" },
+//       {
+//         name: "LinkedIn",
+//         url: "https://www.linkedin.com/in/saheedat-afolabi-1a91b5221/",
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div className="flex h-full">
+//       <Sidebar user={user} />
+//       <div className="container h-screen flex-1 flex flex-col shadow-lg">
+//         <Header />
+//         <div className="main flex-1 p-10 overflow-y-auto">
+//           <Routes>
+//             <Route path="/" element={<RespositoryList username={username} />} />
+//             <Route path="/overview" element={<Overview />} />
+//             <Route path="/repository/:repoId" element={<RepositoryDetails />} />
+//             <Route
+//               path="/random"
+//               element={
+//                 <ErrorBoundary>
+//                   <Random />
+//                 </ErrorBoundary>
+//               }
+//             />
+//             <Route path="/projects" element={<Projects />} />
+//             <Route path="*" element={<Error404 />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
